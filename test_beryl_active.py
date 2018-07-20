b2b0d48ef9e071d6132bf13285fc87c82b855158
Kamil Monticolo <kmonticolo@gmail.com>
2018-07-20 09:25:26 +0200

    add active services test

---
+++ test_beryl_active.py
@@ -0,0 +1,50 @@
+# systemctl list-unit-files | grep service.*enabled |sed 's/.service.*enabled//g' | awk '{print "\""$1"\","}'
+# systemctl list-units --type=service --state=active
+# systemctl list-units --type=service --state=active|grep \.service |sed  's/.service//g' | awk '{print "\""$1"\","}'
+
+def test_serv(host):
+    for spec in (
+"apport",
+"atd",
+"console-setup",
+"cron",
+"dbus",
+"fail2ban",
+"getty@tty1",
+"grub-common",
+"irqbalance",
+"keyboard-setup",
+"kmod-static-nodes",
+"lvm2-lvmetad",
+"lvm2-monitor",
+"networking",
+"ondemand",
+"polkitd",
+"rc-local",
+"resolvconf",
+"rsyslog",
+"setvtrgb",
+"snapd",
+"ssh",
+"systemd-journal-flush",
+"systemd-journald",
+"systemd-logind",
+"systemd-modules-load",
+"systemd-random-seed",
+"systemd-remount-fs",
+"systemd-sysctl",
+"systemd-timesyncd",
+"systemd-tmpfiles-setup-dev",
+"systemd-tmpfiles-setup",
+"systemd-udev-trigger",
+"systemd-udevd",
+"systemd-update-utmp",
+"systemd-user-sessions",
+"ufw",
+"unattended-upgrades",
+"user@1001",
+    ):  
+        service= host.service(spec)
+        assert service.is_running
+
+
