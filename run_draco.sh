475d96447f8ba184e5ae6b331ea3403d30548f1d
Kamil Monticolo <kmonticolo@gmail.com>
2018-07-24 10:17:17 +0200

    add draco

---
+++ run_draco.sh
@@ -0,0 +1,1 @@
+py.test test_draco*.py --ssh-config=/home/kmonti/.ssh/config --hosts draco.artifact.pl
---
+++ test_draco.py
@@ -0,0 +1,110 @@
+# ubuntu 16.04 lts
+
+# userzy grep sh$ /etc/passwd
+##
+#root:x:0:0:root:/root:/bin/bash
+#adam:x:1000:1000:adam,,,:/home/adam:/bin/bash
+#jenkins:x:112:118:Jenkins,,,:/var/lib/jenkins:/bin/bash
+#postgres:x:114:122:PostgreSQL administrator,,,:/var/lib/postgresql:/bin/bash
+#kamilm:x:1001:1001:Kamil M,,,:/home/kamilm:/bin/bash
+#
+
+#ufw
+def test_ufw(Command):
+    command = Command('sudo ufw status | grep -qw active')
+    assert command.rc == 0
+
+def test_cron_running(Process, Service, Socket, Command):
+    assert Service("cron").is_enabled
+    assert Service("cron").is_running
+
+    cron = Process.get(comm="cron")
+    assert cron.user == "root"
+    assert cron.group == "root"
+
+def test_java_running(Process, Service, Socket, Command):
+    cron = Process.filter(comm="java")
+
+def test_munin_running(Process, Service, Socket, Command):
+    assert Service("munin-node").is_enabled
+    assert Service("munin-node").is_running
+
+    munin= Process.get(comm="munin-node")
+    assert munin.user == "root"
+    assert munin.group == "root"
+    assert Socket("tcp://:::4949").is_listening
+
+def test_postgres_running(Process, Service, Socket, Command):
+    assert Service("postgresql").is_enabled
+    assert Service("postgresql").is_running
+
+    postgres = Process.filter(comm="postgres")
+
+    assert Socket("tcp://127.0.0.1:5432").is_listening
+    assert Socket("tcp://::1:5432").is_listening
+
+def test_ufw_running(Process, Service, Socket, Command):
+    assert Service("ufw").is_enabled
+    assert Service("ufw").is_running
+
+def test_zabbix_agent_running(Process, Service, Socket, Command):
+    assert Service("zabbix-agent").is_enabled
+    assert Service("zabbix-agent").is_running
+    assert Socket("tcp://0.0.0.0:10050").is_listening
+
+def test_nginx_running(Process, Service, Socket, Command):
+    assert Service("nginx").is_enabled
+    assert Service("nginx").is_running
+
+    nginx = Process.filter(comm="nginx")
+
+    assert Socket("tcp://0.0.0.0:80").is_listening
+    assert Socket("tcp://:::80").is_listening
+
+
+#fail2ban.service                           enabled 
+def test_fail2ban_running(Process, Service, Socket, Command):
+    assert Service("fail2ban").is_enabled
+    assert Service("fail2ban").is_running
+
+
+
+
+
+# systemctl list-unit-files | grep enabled
+#
+#root@lynx:/home/kamilm# systemctl list-unit-files | grep enabled
+
+
+
+#root@spinel:/home/kamilm#  ls /var/spool/cron/crontabs/
+
+
+# na kazdym firewall ufw ufw status
+
+# root@lynx:/home/kamilm# netstat -alnp|grep LIST|head -20
+# netstat -aln |grep ^tcp.*LIST|awk '{print "\"tcp://"$4"\","}'
+
+##
+def test_listening_socket(host):
+    listening = host.socket.get_listening_sockets()
+    for spec in (
+"tcp://0.0.0.0:80",
+"tcp://0.0.0.0:22",
+"tcp://127.0.0.1:5432",
+"tcp://0.0.0.0:25",
+"tcp://0.0.0.0:443",
+"tcp://0.0.0.0:10050",
+"tcp://:::80",
+"tcp://:::4949",
+"tcp://:::22",
+"tcp://::1:5432",
+"tcp://:::25",
+"tcp://:::9090",
+    ):  
+        socket = host.socket(spec)
+        assert socket.is_listening
+
+
+##procesy
+#root@beryl:/home/kamilm# netstat -alnp|grep LIST|head -20
