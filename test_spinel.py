4a1f5857ae070fdb4a9bbfebadb5e0163ca7650d
kmonticolo <kmonticolo@gmail.com>
2018-07-25 23:55:35 +0200

    cleaning

--- test_spinel.py
+++ test_spinel.py
@@ -124,127 +124,14 @@ def test_gsm_wrapper_pid(host):
 #
 #root@lynx:/home/kamilm# systemctl list-unit-files | grep enabled
 ##
-#cups.service                               enabled 
-#dbus-org.bluez.service                     enabled 
-#dbus-org.freedesktop.Avahi.service         enabled 
-#dbus-org.freedesktop.ModemManager1.service enabled 
-#dbus-org.freedesktop.nm-dispatcher.service enabled 
-#dbus-org.freedesktop.thermald.service      enabled 
-#display-manager.service                    enabled 
-#dns-clean.service                          enabled 
-#fail2ban.service                           enabled 
-#friendly-recovery.service                  enabled 
-#getty@.service                             enabled 
-#gpu-manager.service                        enabled 
-#lightdm.service                            enabled 
-#lvm2-monitor.service                       enabled 
-#ModemManager.service                       enabled 
-#munin-node.service                         enabled 
-#network-manager.service                    enabled 
-#networking.service                         enabled 
-#NetworkManager-dispatcher.service          enabled 
-#NetworkManager-wait-online.service         enabled 
-#NetworkManager.service                     enabled 
-#nginx.service                              enabled 
-#postgresql.service                         enabled 
-#pppd-dns.service                           enabled 
-#resolvconf.service                         enabled 
-#rsyslog.service                            enabled 
-#snapd.autoimport.service                   enabled 
-#snapd.core-fixup.service                   enabled 
-#snapd.seeded.service                       enabled 
-#snapd.service                              enabled 
-#snapd.system-shutdown.service              enabled 
-#ssh.service                                enabled 
-#sshd.service                               enabled 
-#syslog.service                             enabled 
-#systemd-timesyncd.service                  enabled 
-#thermald.service                           enabled 
-#ufw.service                                enabled 
-#unattended-upgrades.service                enabled 
-#ureadahead.service                         enabled 
-#whoopsie.service                           enabled 
-#zabbix-agent.service                       enabled 
-#acpid.socket                               enabled 
-#apport-forward.socket                      enabled 
-#avahi-daemon.socket                        enabled 
-#cups.socket                                enabled 
-#dm-event.socket                            enabled 
-#lvm2-lvmetad.socket                        enabled 
-#lvm2-lvmpolld.socket                       enabled 
-#snapd.socket                               enabled 
-#uuidd.socket                               enabled 
-#remote-fs.target                           enabled 
-#apt-daily-upgrade.timer                    enabled 
-#apt-daily.timer                            enabled 
-#snapd.snap-repair.timer                    enabled 
 ##
 ##systemctl list-units --type=service --state=active
-#apport.service                     loaded active exited  LSB: automatic crash report generation
-#atd.service                        loaded active running Deferred execution scheduler
-#cloud-config.service               loaded active exited  Apply the settings specified in cloud-config
-#cloud-final.service                loaded active exited  Execute cloud user/final scripts
-#cloud-init-local.service           loaded active exited  Initial cloud-init job (pre-networking)
-#cloud-init.service                 loaded active exited  Initial cloud-init job (metadata service crawler)
-#console-setup.service              loaded active exited  Set console font and keymap
-#cron.service                       loaded active running Regular background program processing daemon
-#dbus.service                       loaded active running D-Bus System Message Bus
-#fail2ban.service                   loaded active running Fail2Ban Service
-#getty@tty1.service                 loaded active running Getty on tty1
-#grub-common.service                loaded active exited  LSB: Record successful boot for GRUB
-#ifup@enp1s0.service                loaded active exited  ifup for enp1s0
-#irqbalance.service                 loaded active exited  LSB: daemon to balance interrupts for SMP systems
-#iscsid.service                     loaded active running iSCSI initiator daemon (iscsid)
-#keyboard-setup.service             loaded active exited  Set console keymap
-#kmod-static-nodes.service          loaded active exited  Create list of required static device nodes for the current kernel
-#lvm2-lvmetad.service               loaded active running LVM2 metadata daemon
-#lvm2-monitor.service               loaded active exited  Monitoring of LVM2 mirrors, snapshots etc. using dmeventd or progress polling
-#lxcfs.service                      loaded active running FUSE filesystem for LXC
-#lxd-containers.service             loaded active exited  LXD - container startup/shutdown
-#mdadm.service                      loaded active running LSB: MD monitoring daemon
-#networking.service                 loaded active exited  Raise network interfaces
-#ondemand.service                   loaded active exited  LSB: Set the CPU Frequency Scaling governor to "ondemand"
-#open-iscsi.service                 loaded active exited  Login to default iSCSI targets
-#polkitd.service                    loaded active running Authenticate and Authorize Users to Run Privileged Tasks
-#postfix.service                    loaded active running LSB: Postfix Mail Transport Agent
-#rc-local.service                   loaded active exited  /etc/rc.local Compatibility
-#resolvconf.service                 loaded active exited  Nameserver information manager
-#rsyslog.service                    loaded active running System Logging Service
-#serial-getty@ttyS0.service         loaded active running Serial Getty on ttyS0
-#setvtrgb.service                   loaded active exited  Set console scheme
-#snapd.service                      loaded active running Snappy daemon
-#ssh.service                        loaded active running OpenBSD Secure Shell server
-#systemd-journal-flush.service      loaded active exited  Flush Journal to Persistent Storage
-#systemd-journald.service           loaded active running Journal Service
-#systemd-logind.service             loaded active running Login Service
-#systemd-modules-load.service       loaded active exited  Load Kernel Modules
-#systemd-random-seed.service        loaded active exited  Load/Save Random Seed
-#systemd-remount-fs.service         loaded active exited  Remount Root and Kernel File Systems
-#systemd-sysctl.service             loaded active exited  Apply Kernel Variables
-#systemd-timesyncd.service          loaded active running Network Time Synchronization
-#systemd-tmpfiles-setup-dev.service loaded active exited  Create Static Device Nodes in /dev
-#systemd-tmpfiles-setup.service     loaded active exited  Create Volatile Files and Directories
-#systemd-udev-trigger.service       loaded active exited  udev Coldplug all Devices
-#systemd-udevd.service              loaded active running udev Kernel Device Manager
-#systemd-update-utmp.service        loaded active exited  Update UTMP about System Boot/Shutdown
-#systemd-user-sessions.service      loaded active exited  Permit User Sessions
-#ufw.service                        loaded active exited  Uncomplicated firewall
-#unattended-upgrades.service        loaded active exited  Unattended Upgrades Shutdown
-#user@1000.service                  loaded active running User Manager for UID 1000
-#user@1001.service                  loaded active running User Manager for UID 1001
-#uuidd.service                      loaded active running Daemon for generating UUIDs
-#virtualbox.service                 loaded active exited  LSB: VirtualBox Linux kernel module
-#
-
 
 #root@spinel:/home/kamilm#  ls /var/spool/cron/crontabs/
 #adam  jboss  root
 
 
 # na kazdym firewall ufw ufw status
-
-# root@lynx:/home/kamilm# netstat -alnp|grep LIST|head -20
-# netstat -aln |grep ^tcp.*LIST|awk '{print "\"tcp://"$4"\","}'
 
 ##
 def test_listening_socket(host):
