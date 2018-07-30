7bcec58ffa5ac2ace5b192e37f18382907b500fa
kmonticolo <kmonticolo@gmail.com>
2018-07-30 09:46:31 +0200

    add java ports

--- test_ppos.py
+++ test_ppos.py
@@ -7,6 +7,11 @@ def test_java_running(Process, Service, Socket, Command):
     java = Process.get(comm="java")
     assert java.user == "jboss"
     assert java.group == "jboss"
+    assert Socket("tcp://0.0.0.0:8443").is_listening
+    assert Socket("tcp://0.0.0.0:4447").is_listening
+    assert Socket("tcp://127.0.0.1:9990").is_listening
+    assert Socket("tcp://127.0.0.1:9999").is_listening
+    assert Socket("tcp://0.0.0.0:8080").is_listening
 
 def test_munin_running(Process, Service, Socket, Command):
     assert Service("munin-node").is_enabled
