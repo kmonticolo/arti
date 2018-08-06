0602c63d352d48a1d3f0bb251efcb5a4afd6f8f6
kmonticolo <kmonticolo@gmail.com>
2018-08-06 15:39:03 +0200

    add ufw unchanged check

--- test_alpha.py
+++ test_alpha.py
@@ -1,3 +1,15 @@
+def test_ufw(Command):
+    command = Command('sudo ufw status | grep -qw active')
+    assert command.rc == 0
+
+def test_ufw_running(Process, Service, Socket, Command):
+    assert Service("ufw").is_enabled
+    assert Service("ufw").is_running
+
+def test_ufw_unchanged(Command):
+    command = Command('sudo md5sum /etc/ufw/user.rules')
+    assert command.stdout.rstrip() == 'a4220c5f04d4117ac58ea4ab35480000  /etc/ufw/user.rules'
+    assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
     assert Service("cron").is_enabled
@@ -54,14 +66,6 @@ def test_nginx_running(Process, Service, Socket, Command):
 def test_nginxvalidate(Command):
     command = Command('sudo nginx -t')
     assert command.rc == 0
-
-def test_ufw(Command):
-    command = Command('sudo ufw status | grep -qw active')
-    assert command.rc == 0
-
-def test_ufw_running(Process, Service, Socket, Command):
-    assert Service("ufw").is_enabled
-    assert Service("ufw").is_running
 
 def test_zabbix_agent_running(Process, Service, Socket, Command):
     assert Service("zabbix-agent").is_enabled
