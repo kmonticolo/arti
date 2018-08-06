e2cfd476d59c75f155153ee31bf89ae0391518ce
kmonticolo <kmonticolo@gmail.com>
2018-08-06 15:06:37 +0200

    add ufw unchanged

--- test_diament.py
+++ test_diament.py
@@ -9,6 +9,11 @@ def test_ufw(Command):
     command = Command('sudo ufw status | grep -qw active')
     assert command.rc == 0
 
+def test_ufw_unchanged(Command):
+    command = Command('sudo md5sum /etc/ufw/user.rules')
+    assert command.stdout.rstrip() == '3f2c51641df233bd7e23fe082e9dc054  /etc/ufw/user.rules'
+    assert command.rc == 0
+
 def test_cron_running(Process, Service, Socket, Command):
     assert Service("cron").is_enabled
     assert Service("cron").is_running
