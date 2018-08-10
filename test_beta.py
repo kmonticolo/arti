314bcd2ffff379c41a618d4b8eb6a3ee2082da8c
kmonticolo <kmonticolo@gmail.com>
2018-08-10 08:14:44 +0200

    delete duplicated tests

--- test_beta.py
+++ test_beta.py
@@ -59,46 +59,6 @@ def test_zabbix_agent_running(Process, Service, Socket, Command):
 
 
 # netstat -aln |grep ^tcp.*LIST|awk '{print "\"tcp://"$4"\","}'
-def test_rsyslogd_running(Process, Service, Socket, Command):
-    assert Service("rsyslog").is_enabled
-    assert Service("rsyslog").is_running
-
-def test_crond_running(Process, Service, Socket, Command):
-    assert Service("crond").is_enabled
-    assert Service("crond").is_running
-
-def test_firewalld_running(Process, Service, Socket, Command):
-    assert Service("firewalld").is_enabled
-    assert Service("firewalld").is_running
-
-def test_java_running(Process, Service, Socket, Command):
-    cron = Process.filter(comm="java")
-
-def test_munin_running(Process, Service, Socket, Command):
-    assert Service("munin-node").is_enabled
-    assert Service("munin-node").is_running
-
-
-def test_postfix_running(Process, Service, Socket, Command):
-    assert Service("postfix").is_enabled
-    assert Service("postfix").is_running
-
-    postfix = Process.get(comm="master")
-
-    assert Socket("tcp://127.0.0.1:25").is_listening
-
-def test_tuned_running(Process, Service, Socket, Command):
-    assert Service("tuned").is_enabled
-    assert Service("tuned").is_running
-
-
-def test_zabbix_agent_running(Process, Service, Socket, Command):
-    assert Service("zabbix-agent").is_enabled
-    assert Service("zabbix-agent").is_running
-    assert Socket("tcp://0.0.0.0:10050").is_listening
-
-
-# netstat -aln |grep ^tcp.*LIST|awk '{print "\"tcp://"$4"\","}'
 
 def test_listening_socket(host):
     listening = host.socket.get_listening_sockets()
