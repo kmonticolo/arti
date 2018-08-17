6280ef940ad19a56aef727fe026a29f1da5cd38d
kmonticolo <kmonticolo@gmail.com>
2018-08-17 11:44:11 +0200

    add run_all

---
+++ run_all.sh
@@ -0,0 +1,42 @@
+echo agat
+sh run_agat.sh -q -n4
+echo alpha
+sh run_alpha.sh -q -n4
+echo beryl
+sh run_beryl.sh -q -n4
+echo beta
+sh run_beta.sh -q -n4
+echo db
+sh run_db.sh -q -n4
+echo diament
+sh run_diament.sh -q -n4
+echo draco
+sh run_draco.sh -q -n4
+echo gemini
+sh run_gemini.sh -q -n4
+echo hydra
+sh run_hydra.sh -q -n4
+echo luna 
+sh run_luna.sh -q -n4
+echo lynx
+sh run_lynx.sh -q -n4
+echo ntms
+sh run_ntms.sh -q -n4
+echo orion
+sh run_orion.sh -q -n4
+echo pavo
+sh run_pavo.sh -q -n4
+echo ppos
+sh run_ppos.sh -q -n4
+echo rubin
+sh run_rubin.sh -q -n4
+echo soter
+sh run_soter.sh -q -n4
+echo spinel
+sh run_spinel.sh -q -n4
+echo szmaragd
+sh run_szmaragd.sh -q -n4
+echo taurus
+sh run_taurus.sh -q -n4
+echo topaz
+sh run_topaz.sh -q -n4
