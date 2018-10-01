34ab5ecc9d3fca771ba2b5d21b589682d54aa9a3
kmonticolo <kmonticolo@gmail.com>
2018-10-01 19:24:19 +0200

    add fw rules

--- test_orion.py
+++ test_orion.py
@@ -15,7 +15,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == '602f6d34d35d66f53f7c165667fe3ead  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == '748502586b84dd91fc3a0cab28bc3a21  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
