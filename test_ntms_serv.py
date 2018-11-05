080678e88834ed0e176b49ee568368d92f7934c4
kamil <kamil>
2018-11-05 13:58:33 +0100

    add ntms tests active and services

---
+++ test_ntms_serv.py
@@ -0,0 +1,35 @@
+# systemctl list-unit-files | grep service.*enabled |sed 's/.service.*enabled//g' | awk '{print "\""$1"\","}'
+
+def test_serv(host):
+    for spec in (
+"accounts-daemon",
+"atd",
+"autovt@",
+"cgmanager",
+"cgproxy",
+"cron",
+"dbus-org.freedesktop.thermald",
+"dns-clean",
+"fail2ban",
+"getty@",
+"munin-node",
+"netfilter-persistent",
+"networking",
+"postgresql",
+"pppd-dns",
+"resolvconf",
+"rsyslog",
+"ssh",
+"sshd",
+"syslog",
+"systemd-timesyncd",
+"thermald",
+"ufw",
+"unattended-upgrades",
+"ureadahead",
+"zabbix-agent",
+    ):  
+        service= host.service(spec)
+        assert service.is_enabled
+
+
