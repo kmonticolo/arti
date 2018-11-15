22251d520e1452845f1b772e60cd94b4ed96aa56
kamil <kamil>
2018-11-15 22:31:07 +0100

    add etckeeper clean test

--- test_common.py
+++ test_common.py
@@ -2,4 +2,6 @@ def test_ntp(Command):
     command = Command('ntpstat')
     assert command.rc == 0
 
-
+def test_etckeeper_clean(Command):
+    command = Command('sudo etckeeper vcs status | grep clean$')
+    assert command.rc == 0
