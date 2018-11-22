9799f0205cf7a1d7de9e7bf0c16baac55bc318eb
kamil <kamil>
2018-11-22 09:23:32 +0100

    add 4 web tests

--- test_alpha.py
+++ test_alpha.py
@@ -25,6 +25,26 @@ def test_ntms_rel_website(Command):
     assert command.stdout.rstrip() == '200'
     assert command.rc == 0
 
+def test_ntms_app_website(Command):
+    command = Command('curl -sSfk https://alpha.novelpay.pl -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '200'
+    assert command.rc == 0
+
+def test_PPOS_website(Command):
+    command = Command('curl -sSfk https://alpha.novelpay.pl/PPOS -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '302'
+    assert command.rc == 0
+
+def test_ntms_website(Command):
+    command = Command('curl -sSfk https://alpha.novelpay.pl/ntms -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '301'
+    assert command.rc == 0
+
+def test_ntmss_website(Command):
+    command = Command('curl -sSfk https://alpha.novelpay.pl/ntmss -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '302'
+    assert command.rc == 0
+
 def test_fail2ban_running(Process, Service, Socket, Command):
     assert Service("fail2ban").is_enabled
     assert Service("fail2ban").is_running
