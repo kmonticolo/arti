ddd65c7baeaddfe2278e97bd1108ac33d9604ab2
kamil <kamil>
2018-12-10 21:48:48 +0100

    add nginx validate

--- test_gemini.py
+++ test_gemini.py
@@ -56,6 +56,7 @@ def test_nginx_running(Process, Service, Socket, Command):
     assert Socket("tcp://0.0.0.0:80").is_listening
     assert Socket("tcp://:::80").is_listening
 
+def test_nginx_validate(Command):
     command = Command('sudo nginx -t')
     assert command.rc == 0
 
