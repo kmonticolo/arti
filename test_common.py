60108b36246137a9c61946d409da8069ef7f8327
kamil <kamil>
2018-12-20 12:20:23 +0100

    add test_ufw_default_reject_outgoing

--- test_common.py
+++ test_common.py
@@ -5,3 +5,9 @@ def test_ntp(Command):
 def test_etckeeper_clean(Command):
     command = Command('sudo etckeeper vcs status | grep clean$')
     assert command.rc == 0
+
+def test_ufw_default_reject_outgoing(Command):
+    command = Command('sudo ufw status verbose|grep "reject (outgoing)"')
+    assert command.rc == 0
+
+
