6716868ca4f6ac4756df6cf44c1f84affbd821f5
kamil <kamil>
2018-12-21 09:04:18 +0100

    fix test

--- test_common.py
+++ test_common.py
@@ -6,8 +6,9 @@ def test_etckeeper_clean(Command):
     command = Command('sudo etckeeper vcs status | grep clean$')
     assert command.rc == 0
 
-def test_ufw_default_reject_outgoing(Command):
+def test_ufw_default_reject_outgoing(Command, SystemInfo):
     command = Command('which ufw && sudo ufw status verbose|grep "reject (outgoing)"')
-    assert command.rc == 0
+    if SystemInfo.type == "debian":
+      assert command.rc == 0
 
 
