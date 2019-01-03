42c4898c71afd4fa5157abc6297dd4c3bb4f4096
kamil <kamil>
2019-01-03 10:48:58 +0100

    add fw hub3.artifact.pl

--- test_draco.py
+++ test_draco.py
@@ -17,7 +17,7 @@ def test_ufw(Command):
 # ssh draco.artifact.pl sudo md5sum /etc/ufw/user.rules
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == 'df9750e1344b810e869db23c00bb3b87  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == 'f3403c82bfba447a2455984608df493a  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
