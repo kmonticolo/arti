182ed918757b38559108da16cfca35a596f7db3c
kamil <kamil>
2019-01-25 13:54:28 +0100

    add ssh to alpha

--- test_miki.py
+++ test_miki.py
@@ -56,7 +56,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == 'cd94c37c8f17e52551a3cd242516cfab  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == '67a721317a364400bb025348e8f0fc01  /etc/ufw/user.rules'
 
     assert command.rc == 0
 
