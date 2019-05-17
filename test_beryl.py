c56c6bed0570b074671bb21ceb0c7cddb0c5b99d
kamil <kamil>
2019-05-17 15:01:42 +0200

    fw change

--- test_beryl.py
+++ test_beryl.py
@@ -37,7 +37,7 @@ def test_ufw(Command):
 # ssh beryl.artifact.pl sudo md5sum /etc/ufw/user.rules
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == 'ecb3a22d7a8084adeb80ce7c6b069e08  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == '2d293bd904ac622c7e53b0d74835f569  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
