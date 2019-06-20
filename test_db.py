181740cac79e502e74c6ef31fe8734ec05dc5846
kamil <kamil>
2019-06-20 18:23:21 +0200

    remove munin

--- test_db.py
+++ test_db.py
@@ -31,10 +31,6 @@ def test_crond_running(Process, Service, Socket, Command):
     assert Service("crond").is_enabled
     assert Service("crond").is_running
 
-def test_munin_running(Process, Service, Socket, Command):
-    assert Service("munin-node").is_enabled
-    assert Service("munin-node").is_running
-
 
 def test_postfix_running(Process, Service, Socket, Command):
     assert Service("postfix").is_enabled
@@ -62,7 +58,6 @@ def test_zabbix_agent_running(Process, Service, Socket, Command):
 def test_listening_socket(host):
     listening = host.socket.get_listening_sockets()
     for spec in (
-"tcp://0.0.0.0:4949",
 "tcp://0.0.0.0:22",
 "tcp://127.0.0.1:25",
 "tcp://0.0.0.0:10050",
