f4ff8e728a372c6caab5db8c72b997a0eb3e06b3
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-07-10 10:25:12 +0200

    add kancelaria

--- test_miki.py
+++ test_miki.py
@@ -56,7 +56,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == '43ac0d9e4d2a68a40b615f6659a1f4e2  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == '12e237c04501c0824004df5b26c5dafe  /etc/ufw/user.rules'
 
     assert command.rc == 0
 
