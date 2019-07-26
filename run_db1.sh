ce90e5030d0af8e5aac269d7b71c9a5fb30150f4
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-07-26 11:25:24 +0200

    change db1 to 1db due to interfere with db

--- run_db1.sh
+++ run_db1.sh
@@ -2,4 +2,4 @@
 
 HOST=db1
 SSHCONFIG=./ssh_ntms_config
-py.test test_${HOST}*.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts 192.99.119.27
+py.test test_1db*.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts 192.99.119.27
