103fd31ba4df738cb1705585a51aa4c02ae35cf1
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-07-26 09:23:23 +0200

    add common name to common tests

--- test_common.py
+++ test_common.py
@@ -1,12 +1,12 @@
-def test_ntp(Command):
+def test_common_ntp(Command):
     command = Command('ntpstat')
     assert command.rc == 0
 
-def test_etckeeper_clean(Command):
+def test_common_etckeeper_clean(Command):
     command = Command('sudo etckeeper vcs status | grep clean$')
     assert command.rc == 0
 
-def test_ufw_default_reject_outgoing(Command, SystemInfo):
+def test_common_ufw_default_reject_outgoing(Command, SystemInfo):
     command = Command('which ufw && sudo ufw status verbose|grep "reject (outgoing)"')
     if SystemInfo.type == "debian":
       assert command.rc == 0
