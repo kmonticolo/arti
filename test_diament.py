1990f2b937d010cb383bdce91549d06a18faa52b
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-08-12 22:05:34 +0200

    allow traffic to npntms

--- test_diament.py
+++ test_diament.py
@@ -11,7 +11,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == 'fb9996841a487e7527866b9e510f7c98  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == 'eae0320a2f731e76ca382718e9d02262  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
