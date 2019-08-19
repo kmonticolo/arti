e7e9b631566f9272483a99a18d565ac2b898ca27
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-08-19 16:26:28 +0200

    add amq2

---
+++ test_amq2.py
@@ -0,0 +1,40 @@
+
+def test_ufw(Command):
+    command = Command('sudo ufw status | grep -qw active')
+    assert command.rc == 0
+
+def test_ufw_running(Process, Service, Socket, Command):
+    assert Service("ufw").is_enabled
+    assert Service("ufw").is_running
+
+def test_ufw_unchanged(Command):
+    command = Command('sudo md5sum /etc/ufw/user.rules')
+    assert command.stdout.rstrip() == 'a50bf0c59cd4a5a8b4b325061c669eca  /etc/ufw/user.rules'
+    assert command.rc == 0
+
+def test_cron_running(Process, Service, Socket, Command):
+    assert Service("cron").is_enabled
+    assert Service("cron").is_running
+
+def test_java_running(Process, Service, Socket, Command):
+    java = Process.filter(user="vcs", ppid='1', comm="java")
+
+import pytest
+
+@pytest.mark.parametrize("name,version", [
+    ("python", "2.7"),
+])
+def test_packages(host, name, version):
+    pkg = host.package(name)
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
+
+def test_rsyslogd_running(Process, Service, Socket, Command):
+    assert Service("rsyslog").is_enabled
+    assert Service("rsyslog").is_running
+
+def test_zabbix_agent_running(Process, Service, Socket, Command):
+    assert Service("zabbix-agent").is_enabled
+    assert Service("zabbix-agent").is_running
+    assert Socket("tcp://0.0.0.0:10050").is_listening
+
