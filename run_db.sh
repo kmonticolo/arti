82a5438978e25695f0e4d6413a1dd97e9c15b485
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-08-23 09:17:03 +0200

    remove wildcard from tests to fix db1

--- run_db.sh
+++ run_db.sh
@@ -2,4 +2,5 @@
 
 HOST=db
 SSHCONFIG=/home/kmonti/.ssh/config
-py.test test_${HOST}*.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts ${HOST}.novelpay.pl
+py.test test_${HOST}.py test_${HOST}_active.py test_${HOST}_serv.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts ${HOST}.novelpay.pl
+
