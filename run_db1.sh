5270ea088fccdc1efb2918b973eaaa9d23026135
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-10-02 07:51:36 +0200

    add db1

--- run_db1.sh
+++ run_db1.sh
@@ -2,4 +2,4 @@
 
 HOST=db1
 SSHCONFIG=./ssh_ntms_config
-py.test test_1db*.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts 192.99.119.27
+py.test test_db1*.py test_common.py --ssh-config=${SSHCONFIG} $@ --hosts 192.99.119.27
