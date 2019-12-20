b04fda85097ef5ae8f97d85a8c3e926bde09b2be
Kamil Monticolo <kmonticolo+github@gmail.com>
2019-12-20 20:59:12 +0100

    update ufw

--- test_opal.py
+++ test_opal.py
@@ -4,7 +4,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == 'ad9f459f4eabb2f8aed2c3c327b6e872  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == 'afe2a93f8f0ab769da98807a2971b8a1  /etc/ufw/user.rules'
 
     assert command.rc == 0
 
