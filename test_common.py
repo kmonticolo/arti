ae72461c972727009c716b80b79bc8cd8e9ac5c4
Kamil Monticolo <kmonticolo+github@gmail.com>
2020-05-21 15:27:30 +0200

    modify listening ports

--- test_common.py
+++ test_common.py
@@ -1,7 +1,3 @@
-def test_common_ntp(Command):
-    command = Command('ntpstat')
-    assert command.rc == 0
-
 def test_common_etckeeper_clean(Command):
     command = Command('sudo etckeeper vcs status | grep clean$')
     assert command.rc == 0
