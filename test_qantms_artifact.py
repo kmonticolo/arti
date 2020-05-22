b413c6314c31fedc5cb6aab8f690716844e66f6e
Kamil Monticolo <kmonticolo@gmail.com>
2020-05-22 12:23:19 +0200

    fix qantms artifact test

--- test_qantms_artifact.py
+++ test_qantms_artifact.py
@@ -28,41 +28,15 @@ def test_ufw_running(Process, Service, Socket, Command):
     assert Service("ufw").is_enabled
     assert Service("ufw").is_running
 
-def test_zabbix_server_running(Process, Service, Socket, Command):
-    assert Service("zabbix-server").is_enabled
-    assert Service("zabbix-server").is_running
-    assert Socket("tcp://0.0.0.0:10051").is_listening
-
 def test_zabbix_agent_running(Process, Service, Socket, Command):
     assert Service("zabbix-agent").is_enabled
     assert Service("zabbix-agent").is_running
     assert Socket("tcp://0.0.0.0:10050").is_listening
 
-def test_zookeeper_running(Process, Service, Socket, Command):
-    assert Service("zookeeper").is_enabled
-    assert Service("zookeeper").is_running
-
-def test_zookeeper_conf_unchanged(Command):
-    command = Command('sudo md5sum /opt/zookeeper/conf/zoo.cfg')
-    assert command.stdout.rstrip() == '9881e316b8148213a089953c408b91e2  /opt/zookeeper/conf/zoo.cfg'
-    assert command.rc == 0
-
-def test_mysql_running(Process, Service, Socket, Command):
-    assert Service("mysql").is_enabled
-    assert Service("mysql").is_running
-
-    mysql = Process.get(comm="mysqld_safe")
-    assert mysql.user == "root"
-    assert mysql.group == "root"
-
-    assert Socket("tcp://127.0.0.1:3306").is_listening
-
 #fail2ban.service                           enabled
 def test_fail2ban_running(Process, Service, Socket, Command):
     assert Service("fail2ban").is_enabled
     assert Service("fail2ban").is_running
-
-###
 
 def test_activemq_running(Process, Service, Socket, Command):
     assert Service("activemq").is_enabled
