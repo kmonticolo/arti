791de11af19b3bed8b41dee7ed111d213bb5861d
Kamil Monticolo <kmonticolo@gmail.com>
2020-05-22 13:02:14 +0200

    add qantms test

---
+++ test_qantms.py
@@ -0,0 +1,74 @@
+import pytest
+# z agat
+def test_cron_running(Process, Service, Socket, Command):
+    assert Service("cron").is_enabled
+    assert Service("cron").is_running
+
+def test_postgres_running(Process, Service, Socket, Command):
+    assert Service("postgresql").is_enabled
+    assert Service("postgresql").is_running
+
+    postgres = Process.filter(comm="postgres")
+
+    assert Socket("tcp://127.0.0.1:5432").is_listening
+    assert Socket("tcp://::1:5432").is_listening
+
+def test_ufw_running(Process, Service, Socket, Command):
+    assert Service("ufw").is_enabled
+    assert Service("ufw").is_running
+
+def test_zabbix_server_running(Process, Service, Socket, Command):
+    assert Service("zabbix-server").is_enabled
+    assert Service("zabbix-server").is_running
+    assert Socket("tcp://0.0.0.0:10051").is_listening
+
+def test_zabbix_agent_running(Process, Service, Socket, Command):
+    assert Service("zabbix-agent").is_enabled
+    assert Service("zabbix-agent").is_running
+    assert Socket("tcp://0.0.0.0:10050").is_listening
+# z test_agat
+
+def test_pg_isready_output(Command):
+    command = Command('/usr/bin/pg_isready')
+    assert command.stdout.rstrip() == '/var/run/postgresql:5432 - accepting connections'
+    assert command.rc == 0
+
+def test_nginx_running(Process, Service, Socket, Command):
+    assert Service("nginx").is_enabled
+    assert Service("nginx").is_running
+
+    nginxmaster = Process.get(user="root", ppid='1', comm="nginx")
+    assert nginxmaster.user == "root"
+    assert nginxmaster.group == "root"
+
+    nginxworker = Process.get(ppid=nginxmaster.pid)
+    assert nginxworker.user == "www-data"
+    assert nginxworker.group == "www-data"
+    assert nginxworker.comm == "nginx"
+    assert Socket("tcp://0.0.0.0:80").is_listening
+    assert Socket("tcp://0.0.0.0:443").is_listening
+
+def test_nginx_validate(Command):
+    command = Command('sudo nginx -t')
+    assert command.rc == 0
+# z test_amq1
+def test_java_running(Process, Service, Socket, Command):
+    java = Process.filter(user="vcs", ppid='1', comm="java")
+
+@pytest.mark.parametrize("name,version", [
+    ("python", "2.7"),
+])
+def test_packages(host, name, version):
+    pkg = host.package(name)
+    assert pkg.is_installed
+    assert pkg.version.startswith(version)
+
+def test_rsyslogd_running(Process, Service, Socket, Command):
+    assert Service("rsyslog").is_enabled
+    assert Service("rsyslog").is_running
+# z test_db
+def test_postfix_running(Process, Service, Socket, Command):
+    assert Service("postfix").is_enabled
+    assert Service("postfix").is_running
+    postfix = Process.get(comm="master")
+    assert Socket("tcp://127.0.0.1:25").is_listening
