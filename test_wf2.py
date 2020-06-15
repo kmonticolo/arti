dc7ae82fc6968d8ac3c18b66fc0c6ca13ee7973d
Kamil Monticolo <kmonticolo@gmail.com>
2020-06-15 18:19:12 +0200

    add listening sockets

--- test_wf2.py
+++ test_wf2.py
@@ -44,3 +44,17 @@ def test_zabbix_agent_running(Process, Service, Socket, Command):
     assert Service("zabbix-agent").is_enabled
     assert Service("zabbix-agent").is_running
     assert Socket("tcp://0.0.0.0:10050").is_listening
+
+## netstat -aln |grep ^tcp.*LIST|awk '{print "\"tcp://"$4"\","}'
+def test_listening_socket(host):
+    listening = host.socket.get_listening_sockets()
+    for spec in (
+    "tcp://0.0.0.0:8080",
+    "tcp://127.0.0.53:53",
+    "tcp://0.0.0.0:22",
+    "tcp://0.0.0.0:8443",
+    "tcp://0.0.0.0:10050",
+    "tcp://127.0.0.1:9990",
+        ):
+            socket = host.socket(spec)
+            assert socket.is_listening
