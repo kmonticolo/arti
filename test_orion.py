a0c1988006458ad6d271ea295c49d4d9684cc76e
Kamil Monticolo <kmonticolo+github@gmail.com>
2020-08-11 17:17:36 +0200

    add 172.0.0.0/8 docker temporary workaround

--- test_orion.py
+++ test_orion.py
@@ -19,7 +19,7 @@ def test_ufw(Command):
 
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == '42e5d6640a98ed84630728588b41c43a  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == 'c1306bd4a255f2ce69e1c25395e26fdc  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
