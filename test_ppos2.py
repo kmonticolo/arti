a5224537396691161e2ff63858216554e01e7218
Kamil Monticolo <kmonticolo+github@gmail.com>
2021-05-06 18:27:02 +0200

    add more tests

--- test_ppos2.py
+++ test_ppos2.py
@@ -42,6 +42,11 @@ def test_nginxconf_unchanged(Command):
     assert command.stdout.rstrip() == '5f6e287767d57ec1b82d1c4072448235  /etc/nginx/sites-enabled/ppos2'
     assert command.rc == 0
 
+def test_pproxy_docker_compose_deploy_unchanged(Command):
+    command = Command('sudo md5sum /home/pproxy/pp-releaseprd/docker-compose-deploy.yml')
+    assert command.stdout.rstrip() == 'd4a61109f2273be9691e9c464e71be65  /home/pproxy/pp-releaseprd/docker-compose-deploy.yml'
+    assert command.rc == 0
+
 def test_nginx_validate(Command):
     command = Command('sudo nginx -t')
     assert command.rc == 0
@@ -101,6 +106,59 @@ def test_ppos2_nginx_conf(host):
     assert conf.contains("server_name testlot.novelpay.pl;")
     assert conf.contains("return 301 https://$host$request_uri;")
 
+
+def test_docker_compose_config(host):
+    conf = host.file("/home/pproxy/pp-releaseprd/docker-compose-deploy.yml")
+    assert conf.user == "pproxy"
+    assert conf.group == "pproxy"
+    assert conf.contains("version: \"3.7\"")
+    assert conf.contains("services:")
+    assert conf.contains("  payworld-proxy:")
+    assert conf.contains("    restart: unless-stopped")
+    assert conf.contains("    image: ${PROXY_BACKEND_IMAGE}")
+    assert conf.contains("    container_name: payworld-proxy")
+    assert conf.contains("    ports:")
+    assert conf.contains("      - 0.0.0.0:8081:8080")
+    assert conf.contains("      - 0.0.0.0:5005:5005")
+    assert conf.contains("    env_file:")
+    assert conf.contains("      - compose-setup.env")
+    assert conf.contains("    environment:")
+    assert conf.contains("      - PAYWORLDPROXY_SOAP_KEYSTOREFILE=file:/opt/keystore/payworld-proxy.p12")
+    assert conf.contains("    volumes:")
+    assert conf.contains("      - ${KEYSTORE_PATH}:/opt/keystore/payworld-proxy.p12")
+    assert conf.contains("    networks:")
+    assert conf.contains("      pnet:")
+    assert conf.contains("  payworld-proxy-web:")
+    assert conf.contains("    restart: unless-stopped")
+    assert conf.contains("    image: ${PROXY_UI_IMAGE}")
+    assert conf.contains("    container_name: payworld-proxy-web")
+    assert conf.contains("    ports:")
+    assert conf.contains("      - 0.0.0.0:9090:80")
+    assert conf.contains("    environment:")
+    assert conf.contains("      - NGINX_PORT=80")
+    assert conf.contains("      - PP_RESTAPI_BASE=auto")
+    assert conf.contains("      - PP_NGINX_BE_SCHEME=http")
+    assert conf.contains("      - PP_NGINX_BE_FQDN=payworld-proxy:8080")
+    assert conf.contains("    networks:")
+    assert conf.contains("      pnet:")
+    assert conf.contains("networks:")
+    assert conf.contains("  pnet:")
+    assert conf.contains("    driver: bridge")
+    assert conf.contains("    ipam:")
+    assert conf.contains("      driver: default")
+    assert conf.contains("      config:")
+    assert conf.contains("      - subnet:  10.103.0.1/16")
+
+def test_bpost_website(Command):
+    command = Command('curl -sSfk "https://bpost.novelpay.pl/pp/meta" -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '200'
+    assert command.rc == 0
+
+def test_ppos2_website(Command):
+    command = Command('curl -sSfk "https://ppos2.novelpay.pl/pp/meta" -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '200'
+    assert command.rc == 0
+
 def test_wildfly_standalone_running(Process, Service, Socket, Command):
     standalone = Process.get(user="%s" %username, comm="standalone.sh")
     assert standalone.user == "%s" % username
@@ -158,16 +216,19 @@ def test_listening_socket(host):
 "tcp://0.0.0.0:41616",
 "tcp://0.0.0.0:8080",
 "tcp://0.0.0.0:80",
+"tcp://0.0.0.0:8081", # pproxy docker
 "tcp://127.0.0.53:53",
 "tcp://0.0.0.0:22",
 "tcp://0.0.0.0:5432",
 "tcp://0.0.0.0:8443",
 "tcp://0.0.0.0:443",
+"tcp://0.0.0.0:9090", # pproxy docker
 "tcp://0.0.0.0:10050",
 "tcp://0.0.0.0:35621",
 "tcp://127.0.0.1:9990",
 "tcp://0.0.0.0:35623",
 "tcp://10.103.0.1:27017",
+"tcp://0.0.0.0:5005" # pproxy docker
     ):  
         socket = host.socket(spec)
         assert socket.is_listening
