a64b654b3e7148f3481505465aeb302a8d6936f7
Kamil Monticolo <kmonticolo+github@gmail.com>
2021-07-12 18:01:22 +0200

    remove wildfly as docker is in place, fix ports

--- test_ppos2.py
+++ test_ppos2.py
@@ -98,21 +98,6 @@ def test_ppos2_website(Command):
     assert command.stdout.rstrip() == '200'
     assert command.rc == 0
 
-def test_wildfly_standalone_running(Process, Service, Socket, Command):
-    standalone = Process.get(user="%s" %username, comm="standalone.sh")
-    assert standalone.user == "%s" % username
-    assert standalone.group == "%s" % username
-    java = Process.get(user="%s" %username, comm="java", ppid=standalone.pid)
-    assert java.user == "%s" % username
-    assert java.group == "%s" % username
-    assert Socket("tcp://0.0.0.0:8443").is_listening
-    assert Socket("tcp://127.0.0.1:9990").is_listening
-    assert Socket("tcp://0.0.0.0:8080").is_listening
-
-def test_count_java_process(host):
-    javas = host.process.filter(user="%s" % username, comm="java", fname="java")
-    assert len(javas) == 1
-
 def test_postgres_running(Process, Service, Socket, Command):
     assert Service("postgresql").is_enabled
     assert Service("postgresql").is_running
@@ -142,21 +127,22 @@ def test_mongod_running(Process, Service, Socket, Command):
 def test_listening_socket(host):
     listening = host.socket.get_listening_sockets()
     for spec in (
-"tcp://0.0.0.0:8080",
+"tcp://0.0.0.0:6080", # docker proxy
+"tcp://0.0.0.0:9090", # pproxy docker
+"tcp://0.0.0.0:10050",
+"tcp://0.0.0.0:6787", # docker proxy
+"tcp://0.0.0.0:35621",
+"tcp://0.0.0.0:35623",
+"tcp://0.0.0.0:4200", # docker proxy
+"tcp://10.103.0.1:27017",
+"tcp://0.0.0.0:6443", # docker proxy
+"tcp://0.0.0.0:5005", # pproxy docker
 "tcp://0.0.0.0:80",
 "tcp://0.0.0.0:8081", # pproxy docker
 "tcp://127.0.0.53:53",
 "tcp://0.0.0.0:22",
 "tcp://0.0.0.0:5432",
-"tcp://0.0.0.0:8443",
-"tcp://0.0.0.0:443",
-"tcp://0.0.0.0:9090", # pproxy docker
-"tcp://0.0.0.0:10050",
-"tcp://0.0.0.0:35621",
-"tcp://127.0.0.1:9990",
-"tcp://0.0.0.0:35623",
-"tcp://10.103.0.1:27017",
-"tcp://0.0.0.0:5005" # pproxy docker
+"tcp://0.0.0.0:443"
     ):  
         socket = host.socket(spec)
         assert socket.is_listening
