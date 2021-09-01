6a011646eb3685f985839985b779be6c3139c344
Kamil Monticolo <kmonticolo+github@gmail.com>
2021-09-01 21:05:03 +0200

    fix ufw

--- test_draco.py
+++ test_draco.py
@@ -20,7 +20,7 @@ def test_ufw(Command):
 # ssh draco.artifact.pl sudo md5sum /etc/ufw/user.rules
 def test_ufw_unchanged(Command):
     command = Command('sudo md5sum /etc/ufw/user.rules')
-    assert command.stdout.rstrip() == '11659473ef07ab41f014f51005994071  /etc/ufw/user.rules'
+    assert command.stdout.rstrip() == 'acad214a9981ca7ad4adb8dbbda06688  /etc/ufw/user.rules'
     assert command.rc == 0
 
 def test_cron_running(Process, Service, Socket, Command):
