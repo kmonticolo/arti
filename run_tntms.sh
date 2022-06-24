de50be95201e71e0b1dc1107d528d39ac142f3cc
Kamil Monticolo <kmonticolo+github@gmail.com>
2022-06-24 12:32:42 +0200

    add tntms

---
+++ run_tntms.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HOST=tntms
+SSHCONFIG=/home/kmonti/.ssh/config
+py.test test_${HOST}*.py test_common.py test_ntp.py --ssh-config=${SSHCONFIG} $@ --hosts ${HOST}
---
+++ test_tntms.py
@@ -0,0 +1,47 @@
+def test_goss(Command):
+    command = Command('goss v')
+    assert command.rc == 0
+
+def test_ufw(Command):
+    command = Command('sudo ufw status | grep -w "Status: active"')
+    assert command.rc == 0
+
+def test_ufw_running(Process, Service, Socket, Command):
+    assert Service("ufw").is_enabled
+    assert Service("ufw").is_running
+
+def test_nginx_running(Process, Service, Socket, Command):
+    assert Service("nginx").is_enabled
+    assert Service("nginx").is_running
+
+    assert Socket("tcp://0.0.0.0:80").is_listening
+    assert Socket("tcp://0.0.0.0:443").is_listening
+
+def test_nginx_validate(Command):
+    command = Command('sudo nginx -t')
+    assert command.rc == 0
+
+def test_cron_running(Process, Service, Socket, Command):
+    assert Service("cron").is_enabled
+    assert Service("cron").is_running
+
+def test_postgres_running(Process, Service, Socket, Command):
+    assert Service("postgresql").is_enabled
+    assert Service("postgresql").is_running
+
+    postgres = Process.filter(comm="postgres")
+
+def test_pg_isready_output(Command):
+    command = Command('/usr/bin/pg_isready')
+    assert command.stdout.rstrip() == '/var/run/postgresql:5432 - accepting connections'
+    assert command.rc == 0
+
+def test_redirect_website(Command):
+    command = Command('curl -sSf "http://tntms.novelpay.pl" -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '301' or command.stdout.rstrip() == '302'
+    assert command.rc == 0
+
+def test_https_website(Command):
+    command = Command('curl -ksSf "https://tntms.novelpay.pl" -o /dev/null -w %{http_code}')
+    assert command.stdout.rstrip() == '200'
+    assert command.rc == 0
